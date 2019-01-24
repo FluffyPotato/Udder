40b49006db4df3b57651a4697f1a928cec415c51
Alice Fluffy <fluffy@alices-macbook-pro.local>
2019-01-24 17:28:10 +0100

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Alice Fluffy on 1/24/19.
+//  Copyright © 2019 Alice Fluffy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
